9bbb57487ab5d011be053c08e2b70d7e8cd55cbc
Evan Michael Galloway <evan.galloway@unc.edu>
2018-10-09 09:56:02 -0400

    fixed search index hide filter

--- build.js
+++ build.js
@@ -44,7 +44,7 @@ function parsePost(path){
   return post
 }
 
-const postsForIndexing = posts.map(d=>Object.assign({}, d)).filter(d=>!d.hide);
+const postsForIndexing = posts.map(d=>Object.assign({}, d)).filter(d=>!(d.hide == 'true' ? true : false));
 const searchIndex = JSON.stringify(postsForIndexing.map(ensureDescription));
 fs.writeFileSync(public + '/searchIndex.json', searchIndex);
 
